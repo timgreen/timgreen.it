21e50b7b381e94283767ad896c10e5c4502c75fc
Tim Green <iamtimgreen@gmail.com>
2012-01-30 23:24:51 +0800

    combine css & rename it.

--- nanoc/lib/helpers.rb
+++ nanoc/lib/helpers.rb
@@ -1,10 +1,4 @@
-include Nanoc3::Helpers::Rendering
+include Nanoc3::Helpers::Blogging
 include Nanoc3::Helpers::LinkTo
+include Nanoc3::Helpers::Rendering
 include Nanoc3::Helpers::XMLSitemap
-
-# css
-def less_items
-  items.reject { |i|
-    !((i.identifier =~ /^\/assets\/css\//) && !i[:combined])
-  }
-end
---
+++ nanoc/lib/helpers/assets.rb
@@ -0,0 +1,30 @@
+require 'digest/md5'
+
+module Helpers
+  module Assets
+
+    def digest_for_item(item)
+      digest = Digest::MD5.new
+      digest << item.compiled_content
+      digest.hexdigest
+    end
+
+    # css
+    def less_items(profile)
+      @items.select { |i|
+        (i.identifier.start_with? "/assets/css/#{profile}") && i[:flag].nil?
+      }
+    end
+
+    def combined_css_item(profile)
+      @items.find { |i| i.identifier == "/assets/css/#{profile}/combined/" }
+    end
+
+    def css_digest(profile)
+      digest_for_item(combined_css_item(profile))
+    end
+
+  end
+end
+
+include Helpers::Assets
