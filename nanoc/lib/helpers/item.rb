1417b9ae6a54cea8c86169b7673bdbfd3263ea0a
Tim Green <iamtimgreen@gmail.com>
2012-02-04 02:47:29 +0800

    make itemFor to accept item

--- nanoc/lib/helpers/item.rb
+++ nanoc/lib/helpers/item.rb
@@ -1,10 +1,15 @@
 
-def itemFor(id)
+def item_by_id(id)
   @items.find { |i| i.identifier == id || i.identifier == (id + '/') }
 end
 
-def urlForItem(id)
-  itemFor(id).path
+def itemFor(id_or_item)
+  id_or_item.is_a?(Nanoc3::Item) ? id_or_item : item_by_id(id_or_item)
+end
+
+
+def urlForItem(id_or_item)
+  itemFor(id_or_item).path
 end
 
 def urlForTag(tag)
