b93c0fad87429f499889982563e96573408eddae
Tim Green <iamtimgreen@gmail.com>
2012-02-04 22:53:30 +0800

    +breadcrumbs

--- nanoc/lib/helpers.rb
+++ nanoc/lib/helpers.rb
@@ -1,5 +1,4 @@
 include Nanoc3::Helpers::Blogging
-include Nanoc3::Helpers::LinkTo
 include Nanoc3::Helpers::Rendering
 include Nanoc3::Helpers::Tagging
 include Nanoc3::Helpers::XMLSitemap
--- nanoc/lib/helpers/item.rb
+++ nanoc/lib/helpers/item.rb
@@ -4,7 +4,7 @@ def item_by_id(id)
 end
 
 def itemFor(id_or_item)
-  id_or_item.is_a?(Nanoc3::Item) ? id_or_item : item_by_id(id_or_item)
+  (id_or_item.is_a?(Nanoc3::Item) || id_or_item.is_a?(Nanoc3::ItemRep)) ? id_or_item : item_by_id(id_or_item)
 end
 
 
@@ -14,4 +14,40 @@ end
 
 def urlForTag(tag)
   urlForItem("/tags/#{tag}")
+end
+
+def breadcrumbs_for(id_or_item)
+  i = itemFor(id_or_item)
+  if i.identifier == '/'
+    []
+  else
+    breadcrumbs_for(i.identifier.sub(/[^\/]+\/$/, '')) + [ i ]
+  end
+end
+
+def link_to(text, target, attributes={})
+  # Find path
+  if target.is_a?(String)
+    url = target
+  else
+    url = urlForItem(target)
+    raise RuntimeError, "Cannot create a link to #{target.inspect} because this target is not outputted (its routing rule returns nil)" if url.nil?
+  end
+
+  # Join attributes
+  attributes = attributes.inject('') do |memo, (key, value)|
+    memo + key.to_s + '="' + h(value) + '" '
+  end
+
+  # Create link
+  "<a #{attributes}href=\"#{h url}\">#{text}</a>"
+end
+
+def link_to_unless_current(text, target, attributes={})
+  if @item_rep && urlForItem(@item_rep) == urlForItem(target)
+    # Create message
+    "<span class=\"active\" title=\"You're here.\">#{text}</span>"
+  else
+    link_to(text, target, attributes)
+  end
 end
